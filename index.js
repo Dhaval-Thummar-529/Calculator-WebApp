5c2fdec10c37b17347532ede4b9c05e91244f628
Dhaval-Thummar-529 <190304105062@paruluniversity.ac.in>
2022-01-13 22:27:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+
+function B7(){
+    var b7 = document.getElementById("b7").value
+    var d = document.getElementById("display");
+    d.value = d.value + b7
+}
+function B8(){
+    var b8 = document.getElementById("b8").value
+    var d = document.getElementById("display");
+    d.value = d.value + b8
+}
+function B9(){
+    var b9 = document.getElementById("b9").value
+    var d = document.getElementById("display");
+    d.value = d.value + b9
+}
+function B4(){
+    var b4 = document.getElementById("b4").value
+    var d = document.getElementById("display");
+    d.value = d.value + b4
+}
+function B5(){
+    var b5 = document.getElementById("b5").value
+    var d = document.getElementById("display");
+    d.value = d.value + b5
+}
+function B6(){
+    var b6 = document.getElementById("b6").value
+    var d = document.getElementById("display");
+    d.value = d.value + b6
+}
+function B1(){
+    var b1 = document.getElementById("b1").value
+    var d = document.getElementById("display");
+    d.value = d.value + b1
+}
+function B2(){
+    var b2 = document.getElementById("b2").value
+    var d = document.getElementById("display");
+    d.value = d.value + b2
+}
+function B3(){
+    var b3 = document.getElementById("b3").value
+    var d = document.getElementById("display");
+    d.value = d.value + b3
+}
+function B0(){
+    var b0 = document.getElementById("b0").value
+    var d = document.getElementById("display");
+    d.value = d.value + b0
+}
+function dec(){
+    var dec = document.getElementById("decimal").value
+    var d = document.getElementById("display");
+    d.value = d.value + dec
+    dec.disabled = true
+}
+
+function Clr(){
+    var c = document.getElementById("clear").value
+    var d = document.getElementById("display");
+    d.value = c
+}
